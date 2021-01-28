ec3848f8ebd41c94e06c6549f87ec8da1d6e9250
kojix2 <2xijok@gmail.com>
2021-01-28 15:42:11 +0900

    Add tests * Mapopt, IdxSeq, Idx, IdxReader, Extra

--- test/minimap2/ffi_test.rb
+++ test/minimap2/ffi_test.rb
@@ -37,5 +37,100 @@ class FFITest < Minitest::Test
     assert_equal true, obj[:h].null?
   end
 
-  def test_mapopt; end
+  def test_mapopt
+    obj = MM2::FFI::Mapopt.new
+    assert_instance_of MM2::FFI::Mapopt, obj
+    assert_equal 0, obj[:flag]
+    assert_equal 0, obj[:seed]
+    assert_equal 0, obj[:sdust_thres]
+    assert_equal 0, obj[:max_qlen]
+    assert_equal 0, obj[:bw]
+    assert_equal 0, obj[:max_gap]
+    assert_equal 0, obj[:max_gap_ref]
+    assert_equal 0, obj[:max_frag_len]
+    assert_equal 0, obj[:max_chain_skip]
+    assert_equal 0, obj[:max_chain_iter]
+    assert_equal 0, obj[:min_cnt]
+    assert_equal 0, obj[:min_chain_score]
+    assert_equal 0, obj[:chain_gap_scale]
+    assert_equal 0, obj[:mask_level]
+    assert_equal 0, obj[:mask_len]
+    assert_equal 0, obj[:pri_ratio]
+    assert_equal 0, obj[:best_n]
+    assert_equal 0, obj[:max_join_long]
+    assert_equal 0, obj[:max_join_short]
+    assert_equal 0, obj[:min_join_flank_sc]
+    assert_equal 0, obj[:min_join_flank_ratio]
+    assert_equal 0, obj[:alt_drop]
+    assert_equal 0, obj[:a]
+    assert_equal 0, obj[:b]
+    assert_equal 0, obj[:q]
+    assert_equal 0, obj[:e]
+    assert_equal 0, obj[:q2]
+    assert_equal 0, obj[:e2]
+    assert_equal 0, obj[:sc_ambi]
+    assert_equal 0, obj[:noncan]
+    assert_equal 0, obj[:junc_bonus]
+    assert_equal 0, obj[:zdrop]
+    assert_equal 0, obj[:zdrop_inv]
+    assert_equal 0, obj[:end_bonus]
+    assert_equal 0, obj[:min_dp_max]
+    assert_equal 0, obj[:min_ksw_len]
+    assert_equal 0, obj[:anchor_ext_len]
+    assert_equal 0, obj[:anchor_ext_shift]
+    assert_equal 0, obj[:max_clip_ratio]
+    assert_equal 0, obj[:pe_ori]
+    assert_equal 0, obj[:pe_bonus]
+    assert_equal 0, obj[:mid_occ_frac]
+    assert_equal 0, obj[:min_mid_occ]
+    assert_equal 0, obj[:mid_occ]
+    assert_equal 0, obj[:max_occ]
+    assert_equal 0, obj[:mini_batch_size]
+    assert_equal 0, obj[:max_sw_mat]
+    assert_nil obj[:split_prefix]
+  end
+
+  def test_idxseq
+    obj = MM2::FFI::IdxSeq.new
+    assert_instance_of MM2::FFI::IdxSeq, obj
+    assert_nil obj[:name]
+    assert_equal 0, obj[:offset]
+    assert_equal 0, obj[:len]
+  end
+
+  def test_idx
+    obj = MM2::FFI::Idx.new
+    assert_instance_of MM2::FFI::Idx, obj
+    assert_equal 0, obj[:b]
+    assert_equal 0, obj[:w]
+    assert_equal 0, obj[:flag]
+    assert_equal 0, obj[:n_seq]
+    assert_equal true, obj[:seq].null?
+    assert_equal true, obj[:S].null?
+    assert_equal true, obj[:B].null?
+    assert_equal true, obj[:km].null?
+    assert_equal true, obj[:h].null?
+  end
+
+  def test_Reader
+    obj = MM2::FFI::IdxReader.new
+    assert_instance_of MM2::FFI::IdxReader, obj
+    assert_equal 0, obj[:is_idx]
+    assert_equal 0, obj[:n_parts]
+    assert_equal 0, obj[:idx_size]
+    assert_instance_of MM2::FFI::Idxopt, obj[:opt]
+    assert_equal true, obj[:fp_out].null?
+    assert_equal true, obj[:seq_or_idx].null?
+  end
+
+  def test_extra
+    obj = MM2::FFI::Extra.new
+    assert_instance_of MM2::FFI::Extra, obj
+    assert_equal 0, obj[:capacity]
+    assert_equal 0, obj[:dp_score]
+    assert_equal 0, obj[:dp_max]
+    assert_equal 0, obj[:dp_max2]
+    assert_equal 0, obj[:n_ambi_trans_strand]
+    assert_equal 0, obj[:n_cigar]
+  end
 end
