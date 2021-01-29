345b58bf0fd8efc501415521d220c0fb7bf50a3b
kojix2 <2xijok@gmail.com>
2021-01-29 12:20:24 +0900

    Improved alignment

--- lib/minimap2/alignment.rb
+++ lib/minimap2/alignment.rb
@@ -1,7 +1,22 @@
 module Minimap2
-  class Alignment
-    def initialize; end
+  class Alignment 
+    def self.keys
+      %i[ctg ctg_len r_st r_en strand trans_strand blen mlen nm primary
+       q_st q_en mapq cigar read_num cs md cigar_str]
+    end
+
+    # Read only
+    attr_reader(*keys)
+
+    def initialize
+    end
 
     def destroy; end
+
+    def is_primary?
+    end
+
+    def to_s
+    end
   end
 end
