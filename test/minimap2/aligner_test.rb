4c07beceebad677335eb6a5630051f16a41b95c0
kojix2 <2xijok@gmail.com>
2021-03-13 22:07:52 +0900

    Improved aligner test

--- test/minimap2/aligner_test.rb
+++ test/minimap2/aligner_test.rb
@@ -11,39 +11,24 @@ class AlignerTest < Minitest::Test
     assert_instance_of MM2::Aligner, @a
   end
 
+  def test_idx_opt
+    assert_instance_of MM2::FFI::IdxOpt, @a.idx_opt
+  end
+  
+  def test_map_opt
+    assert_instance_of MM2::FFI::MapOpt, @a.map_opt
+  end
+
+  def test_index
+    assert_instance_of MM2::FFI::Idx, @a.index
+  end
+
   def test_seq
     assert_nil @a.seq("MT_human", 0, 0)
     assert_equal "G", @a.seq("MT_human", 0, 1)
     assert_equal "GA", @a.seq("MT_human", 0, 2)
     assert_equal "CACAG", @a.seq("MT_human", 3, 8)
     assert_equal "ATCACGATG", @a.seq("MT_human", 16_560)
-  end
-
-  def test_align
-    seq = @a.seq("MT_human", 100, 300)
-    hit = {
-      ctg: "MT_human",
-      ctg_len: 16_569,
-      r_st: 100,
-      r_en: 300,
-      strand: 1,
-      trans_strand: 0,
-      blen: 200,
-      mlen: 200,
-      nm: 0,
-      primary: 1,
-      q_st: 0,
-      q_en: 200,
-      mapq: 60,
-      cigar: [[200, 0]],
-      read_num: 1,
-      cs: "",
-      md: "",
-      cigar_str: "200M"
-    }
-    @a.align(seq) do |h|
-      assert_equal hit, h.to_h
-    end
   end
 
   def test_k
