fa69e104ca0102e0da1edf2d0fa5511f0ba64f89
kojix2 <2xijok@gmail.com>
2023-04-27 10:48:47 +0900

    Fix quick_start example

--- examples/quick_start.rb
+++ examples/quick_start.rb
@@ -3,12 +3,13 @@
 require "minimap2"
 
 # load or build index
-aligner = Minimap2::Aligner.new("minimap2/test/MT-human.fa")
+aligner = Minimap2::Aligner.new("#{__dir__}/../ext/minimap2/test/MT-human.fa")
 
 # retrieve a subsequence from the index
 seq = aligner.seq("MT_human", 100, 200)
 
 # mapping
-aligner.align(seq) do |h|
-  pp h.to_h
-end
+hits = aligner.align(seq)
+
+# show result
+pp hits
