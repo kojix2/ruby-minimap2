ef136ea660200d141f7425fb1aa07bf52b5ea8b4
kojix2 <2xijok@gmail.com>
2023-05-01 03:11:29 +0000

    Fixing Regular Expressions in Test

--- test/minimap2_test.rb
+++ test/minimap2_test.rb
@@ -22,7 +22,7 @@ class Minimap2Test < Minitest::Test
     rescue NotImplementedError
       skip "Fork not supported on this platform"
     end
-    assert_match(/^[\d,.,\-,r]+\n$/, out)
+    assert_match(/^[\d\.\-r]+\n/, out)
     assert_equal true, Minimap2::VERSION.include?(out.split("-r")[0])
     assert_equal "", err
   end
