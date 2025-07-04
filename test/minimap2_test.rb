47610f86053b814fe15e0d43d225b6857392f371
kojix2 <2xijok@gmail.com>
2025-07-04 13:22:30 +0900

    Refactor: Move MM2 constant definition to test_helper.rb

--- test/minimap2_test.rb
+++ test/minimap2_test.rb
@@ -2,8 +2,6 @@
 
 require "test_helper"
 
-MM2 = Minimap2
-
 class Minimap2Test < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Minimap2::VERSION
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,7 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "minimap2"
 
+MM2 = Minimap2
+
 require "minitest/autorun"
 require "minitest/pride"
